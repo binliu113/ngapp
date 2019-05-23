a86f8123a67d8ca2852c50888e7dba555f5f5825
liu <lb18740599813@126.com>
2019-05-23 22:04:06 +0800

    sdafsd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'app';
-=======
-  title = 'Tour of Heroes';
->>>>>>> 0a5dddd27859e67e3374256b0664784c37c33297
 }
